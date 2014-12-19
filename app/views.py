6c8cf22d7a9161f42fd5f74dfff81c240fa7f3ab
darknawg63 <darknawg63@gmail.com>
2014-12-19 13:43:33 +0100

    View embedded html

--- app/views.py
+++ app/views.py
@@ -3,5 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index/')
 def index():
-	greeting = {"greet" :"Hello, World!"}
-    return greeting['greet']
+	user = {'nickname': 'Miguel'}  # fake user
+	return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
