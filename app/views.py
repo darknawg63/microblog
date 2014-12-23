26f735b1b43a2199129a47893dc1db1e5a9acecd
darknawg63 <darknawg63@gmail.com>
2014-12-23 22:28:47 +0100

    Added user profile skeleton

--- app/views.py
+++ app/views.py
@@ -52,6 +52,21 @@ def login():
                            providers=app.config['OPENID_PROVIDERS'])
 
 
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    if user == None:
+        flash('User %s not found.' % nickname)
+        return redirect(url_for('index'))
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html',
+                           user=user,
+                           posts=posts)
+
 @oid.after_login
 def after_login(resp):
     if resp.email is None or resp.email == "":
